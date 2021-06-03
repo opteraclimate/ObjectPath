075be4288e4581ddb7104ee222b9522fa0248677
Ilya Sukhanov <ilya@sukhanov.net>
2021-06-03 13:37:05 -0600

    Add a boolean operator

--- objectpath/core/interpreter.py
+++ objectpath/core/interpreter.py
@@ -571,6 +571,8 @@ class Tree(Debugger):
           return float(args[0])
         elif fnName == "str":
           return str(py2JSON(args[0]))
+        elif fnName == "bool":
+          return bool(args[0])
         elif fnName in ("list", "array"):
           try:
             a = args[0]
